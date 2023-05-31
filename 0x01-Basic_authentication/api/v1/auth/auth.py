e2bfebed4463b4aecbf34a44d497d09c0955710a
calvean <garnetsharara@gmail.com>
2023-05-31 17:43:30 +0200

    task 0

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,11 +6,15 @@ from typing import List, TypeVar
 
 
 class Auth:
+    """ Authentication class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ reqiure Authentication method """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """ method for authentication header """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """ method to return the user """
         return None
