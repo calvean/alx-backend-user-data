83507c878baf9cc0c9f85747c458cc7cd43a9139
calvean <garnetsharara@gmail.com>
2023-05-31 18:21:05 +0200

    several fixes

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,10 +21,14 @@ if getenv("AUTH_TYPE"):
 
 @app.before_request
 def before_request():
+    """ execute before request """
     if auth is None:
         return
 
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = [
+      '/api/v1/status/',
+      '/api/v1/unauthorized/',
+      '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
         return
 
@@ -59,4 +63,3 @@ if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-
