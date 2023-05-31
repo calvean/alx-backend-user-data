3fd4c2179153a00fc356b2ee5bb29f52e6190505
calvean <garnetsharara@gmail.com>
2023-05-31 17:47:55 +0200

    fixes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,8 +8,14 @@ from typing import List, TypeVar
 class Auth:
     """ Authentication class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ reqiure Authentication method """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for excluded_path in excluded_paths:
+            if path.endswith(excluded_path):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ method for authentication header """
