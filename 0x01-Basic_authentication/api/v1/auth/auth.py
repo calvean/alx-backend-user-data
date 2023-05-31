c586392e106bb5063c488d730487a2bd4a5c5f40
calvean <garnetsharara@gmail.com>
2023-05-31 17:49:16 +0200

    fixes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,6 +8,7 @@ from typing import List, TypeVar
 class Auth:
     """ Authentication class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require authenticaion """
         if path is None:
             return True
         if excluded_paths is None or len(excluded_paths) == 0:
