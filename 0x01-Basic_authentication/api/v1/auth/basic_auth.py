c4501f976ed0fd89bd817754a1af078f9eec207d
calvean <garnetsharara@gmail.com>
2023-06-01 14:48:50 +0200

    finor changes

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -66,9 +66,8 @@ class BasicAuth(Auth):
         if len(users) == 0:
             return None
 
-        user = users[0]
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
 
-        if not user.is_valid_password(user_pwd):
-            return None
-
-        return user
+        return None
