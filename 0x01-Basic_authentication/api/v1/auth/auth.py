ca3fb24092f650fb4c30954b6f560b2d2574221d
calvean <garnetsharara@gmail.com>
2023-06-01 15:34:23 +0200

    changes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,18 +3,20 @@
 """
 from flask import request
 from typing import List, TypeVar
+import fnmatch
 
 
 class Auth:
     """ Authentication class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ require authentication """
+        """ Checks if authentication is required for the given path
+        """
         if path is None:
             return True
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
-        for excluded_path in excluded_paths:
-            if path.rstrip('/') == excluded_path.rstrip('/'):
+        for pattern in excluded_paths:
+            if fnmatch.fnmatch(path, pattern):
                 return False
         return True
 
