57510c9407cea5a9645662eb9c716e169f774f18
calvean <garnetsharara@gmail.com>
2023-06-01 17:40:45 +0200

    session_auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,18 @@
 """ Module of Session Authentication
 """
 from api.v1.auth.auth import Auth
+import uuid
+from typing import Optional
 
 
 class SessionAuth(Auth):
     """ Session Authentication Class """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> Optional[str]:
+        """ Create a Session ID for a user_id """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
