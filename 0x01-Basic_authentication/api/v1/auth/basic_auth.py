a541ec415fae246167ca1e2e8c8b273502ea2a34
calvean <garnetsharara@gmail.com>
2023-06-01 14:16:25 +0200

    removed whitespaces

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,12 +6,16 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ Basic Authentication Class """
-        def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        if authorization_header is None or not isinstance(authorization_header, str):
+    def extract_base64_authorization_header(
+      self,
+      authorization_header: str) -> str:
+        """ base64 authorization header """
+        if authorization_header is None or not isinstance(
+          authorization_header,
+          str):
             return None
-        
+
         if not authorization_header.startswith('Basic '):
             return None
-        
+
         return authorization_header.split(' ')[1]
-    pass
