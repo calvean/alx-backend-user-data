6a74a8f18692903ca91e6fb49c628ff109e843aa
calvean <garnetsharara@gmail.com>
2023-06-01 14:19:47 +0200

    fixes

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -19,3 +19,17 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(
+      self,
+      base64_authorization_header: str) -> str:
+        if base64_authorization_header is None or not isinstance(
+          base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+        except base64.binascii.Error:
+            return None
