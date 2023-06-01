601e48a802ac4a4aa5c30e80c3bb803517923d81
calvean <garnetsharara@gmail.com>
2023-06-01 15:02:00 +0200

    changes

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -71,3 +71,24 @@ class BasicAuth(Auth):
                 return user
 
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Retrieves the User instance for a request
+        """
+        if request is None:
+            return None
+        authorization_header = self.authorization_header(request)
+        if authorization_header is None:
+            return None
+        base64_header = self.extract_base64_authorization_header(
+          authorization_header)
+        if base64_header is None:
+            return None
+        decoded_header = self.decode_base64_authorization_header(base64_header)
+        if decoded_header is None:
+            return None
+        email, password = self.extract_user_credentials(decoded_header)
+        if email is None or password is None:
+            return None
+        user = self.user_object_from_credentials(email, password)
+        return user
