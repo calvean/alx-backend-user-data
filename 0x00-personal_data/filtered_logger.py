7ffe909d073f754b89af793f9f23cb6ffa7d44b8
calvean <garnetsharara@gmail.com>
2023-06-08 15:01:51 +0200

    added readme

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 """ Regexing Module """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+  fields: List[str],
+  redaction: str,
+  message: str,
+  separator: str) -> str:
+    """
+    Obfuscates specified fields in a log message.
+
+    Arguments:
+        fields: list of strings representing fields to obfuscate.
+        redaction: string representing value to replace
+        message: string representing the log line.
+        separator: string separating the fields in the log line.
+
+    Returns:
+        The obfuscated log message.
+    """
     regex_pattern = r'({})'.format('|'.join(map(re.escape, fields)))
-    return re.sub(regex_pattern, redaction, message)
+    obfuscated_message = re.sub(regex_pattern, redaction, message)
+
+    return obfuscated_message
