2a21c176bb7413531249c70857c2407fbc0b13ea
calvean <garnetsharara@gmail.com>
2023-06-08 14:47:09 +0200

    created fillter dutum

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,7 +2,7 @@
 """ Regexing Module """
 import re
 
+
 def filter_datum(fields, redaction, message, separator):
     regex_pattern = r'({})'.format('|'.join(map(re.escape, fields)))
     return re.sub(regex_pattern, redaction, message)
-
